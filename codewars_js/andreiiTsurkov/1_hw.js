b196804ad8f146b045240fa0c81a37165ab919ae
Andrii Tsurkov <daroga@ukr.net>
2023-11-19 19:04:11 +0200

    modified:   1_hw.js

--- codewars_js/andreiiTsurkov/1_hw.js
+++ codewars_js/andreiiTsurkov/1_hw.js
@@ -1,32 +1,47 @@
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
-}
+// function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+// }
 
-function makeNegative(num) {
-    if (num > 0) {
-        return -1 * num
-    }
-    return num
-}
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) =>
+  laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 
-function move(position, roll) {
-    return position + (roll * 2)
-}
 
-function greet(name, owner) {
-    if (name === owner) {
-        return 'Hello boss'
-    }
-    return 'Hello guest'
-}
+// function makeNegative(num) {
+//     if (num > 0) {
+//         return -1 * num
+//     }
+//     return num
+// }
 
-function litres(time) {
-    return Math.floor(time * 0.5);
-}
+const makeNegative = num => (num > 0 ? -1 * num : num);
 
-function lovefunc(flower1, flower2) {
-    if (flower1 % 2 != flower2 % 2) {
-        return true;
-    }
-    return false;
-}
+// function move(position, roll) {
+//     return position + (roll * 2)
+// }
+
+const move = (position, roll) => position + (roll * 2);
+
+// function greet(name, owner) {
+//     if (name === owner) {
+//         return 'Hello boss'
+//     }
+//     return 'Hello guest'
+// }
+
+const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest';
+
+
+// function litres(time) {
+//     return Math.floor(time * 0.5);
+// }
+
+const litres = time => Math.floor(time * 0.5);
+
+// function lovefunc(flower1, flower2) {
+//     if (flower1 % 2 != flower2 % 2) {
+//         return true;
+//     }
+//     return false;
+// }
+
+const lovefunc = (flower1, flower2) => flower1 % 2 != flower2 % 2;
