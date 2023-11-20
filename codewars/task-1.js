a7d5d45b3c127edc6b4f0ff300b8136b6d271079
Arsenii Danylenko <103411610+ArsenDanylenko@users.noreply.github.com>
2023-11-20 01:36:34 +0000

    added my solutions o js tasks folder

--- codewars/task-1.js
+++ codewars/task-1.js
@@ -4,17 +4,24 @@
 const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) =>
   laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 
+// NextName and code
+
 
 // 2 Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript //
 
 // Andrii Tsurkov
 const makeNegative = num => num > 0 ? -1 * num : num;
 
+// NextName and code
+// Arsenii Danylenko
+// const makeNegative = (num) => num <= 0 ? num : -num;
 
 // 3 Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript //
 
 // Andrii Tsurkov
 const move = (position, roll) => position + (roll * 2);
+
+// NextName and code
 
 
 // 4 Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript //
@@ -22,11 +29,15 @@ const move = (position, roll) => position + (roll * 2);
 // Andrii Tsurkov
 const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest';
 
+// NextName and code
+
 
 // 5 Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript //
 
 // Andrii Tsurkov
 const litres = time => Math.floor(time * 0.5);
+
+// NextName and code
 
 
 // 6 Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript //
@@ -34,5 +45,8 @@ const litres = time => Math.floor(time * 0.5);
 // Andrii Tsurkov
 const lovefunc = (flower1, flower2) => flower1 % 2 != flower2 % 2;
 
+// NextName and code
+// Arsenii Danylenko
+// const lovefunc = (flower1, flower2) => flower1 % 2 !== flower2 % 2;
 
 
