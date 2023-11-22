f346d9e3ebd66a1c839340df1f0a22f42eb75a84
JulieUst <149378133+JulieUst@users.noreply.github.com>
2023-11-22 09:31:38 +0000

    Commit tasks

--- codewars/task-2.js
+++ codewars/task-2.js
@@ -16,3 +16,34 @@ function min(arr, toReturn) {
   }
   return arr.indexOf(minValue);
 }
+
+
+
+
+
+//https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+function warnTheSheep(queue) {
+  const wolfIndex = queue.findIndex(animal => animal === 'wolf');
+  
+  if (wolfIndex === queue.length - 1) {
+      return "Pls go away and stop eating my sheep";
+  } else {
+      const sheepNumber = queue.length - 1 - wolfIndex;
+      return `Oi! Sheep number ${sheepNumber}! You are about to be eaten by a wolf!`;
+  }
+}
+
+//https://www.codewars.com/kata/beginner-lost-without-a-map
+function maps(arr) {
+  return arr.map(num => num * 2);
+}
+
+https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+function firstNonConsecutive(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1] + 1) {
+      return arr[i];
+    }
+  }
+  return null;
+}
