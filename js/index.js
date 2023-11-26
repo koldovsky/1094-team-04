1ceb98e4cebcc217a33f4a91a5e3c2727c77161a
Arsenii Danylenko <103411610+ArsenDanylenko@users.noreply.github.com>
2023-11-26 17:41:17 +0000

    transferred my js code into respective files and folders and imported them into index.js

--- js/index.js
+++ js/index.js
@@ -1,53 +1,5 @@
 document.addEventListener("partialsLoaded", () => {
-
-    // ==== Burger menu ====
-    const body = document.querySelector('body');
-    const burgerBtn = document.querySelector('.header__burger-btn');
-    const menu = document.querySelector('.header__menu');
-    const menuItems = document.querySelectorAll('.menu__item');
-
-    if (burgerBtn) {
-        const toggleActiveClasses = () => {
-            burgerBtn.classList.toggle('_active');
-            menu.classList.toggle('_active');
-            body.classList.toggle('fixed');
-        };
-
-        const handleBurgerClick = () => {
-            toggleActiveClasses();
-        };
-
-        burgerBtn.addEventListener('click', handleBurgerClick);
-    }
-
-    for (const menuItem of menuItems) {
-        menuItem.addEventListener('click', () => {
-            burgerBtn.classList.remove('_active');
-            menu.classList.remove('_active');
-            body.classList.remove('fixed');
-        })
-    }
-
-    // ==== Initializing Swiper carousel ====
-    const swiper = new Swiper('.animal-cards__carousel', {
-        direction: 'horizontal',
-        loop: true,
-        speed: 400,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        spaceBetween: 30,
-        breakpoints: {
-            576: {
-                slidesPerView: 2
-            },
-            992: {
-                slidesPerView: 4
-            }
-        },
-        navigation: {
-            prevEl: '.animal-cards__carousel-btn-prev',
-            nextEl: '.animal-cards__carousel-btn-next',
-        },
-    });
+    import('./header/burger-menu.js');
+    import('./index.animal-cards-carousel.js');
 });
 
