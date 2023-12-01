06a142167b7241e28acc810f4a8d35e4c42791c5
Andrii Tsurkov <daroga@ukr.net>
2023-12-01 23:23:44 +0200

    new file:   html-blocks/index.brand-cards.partal.html 	modified:   index.html 	new file:   js/index.brand-cards-carousel.js 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -2,5 +2,6 @@ document.addEventListener("partialsLoaded", () => {
     import('./header/burger-menu.js');
     import('./index.animal-cards-carousel.js');
     import('./index.faq-accordion.js');
+    import('./index.brand-cards-carousel.js');
 });
 
